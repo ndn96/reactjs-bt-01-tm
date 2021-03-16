fb1020b599642390dc2e8ed403e44caa68d28433
Nguyễn Danh Nam <dev.danhnam@gmail.com>
2021-03-16 19:08:23 +0700

    Bài tập 01 - ReactJs - Techmaster

---
+++ src/components/HeaderContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function HeaderContainer(props) {
+  return (
+    <>
+      <header className="container">
+        <h1>Shopping Cart</h1>
+        <ul className="breadcrumb">
+          <li>Home</li>
+          <li>Shopping Cart</li>
+        </ul>
+        <span className="count">3 items in the bag</span>
+      </header>
+    </>)
+}
